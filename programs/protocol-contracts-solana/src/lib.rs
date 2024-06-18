aa4f220fac4fa690a23a5350f07a8ddd450cfde1
brewmaster012 <88689859+brewmaster012@users.noreply.github.com>
2024-06-18 16:06:21 -0500

    add a validation prototype for ECDSA signature verification

--- programs/protocol-contracts-solana/src/lib.rs
+++ programs/protocol-contracts-solana/src/lib.rs
@@ -1,15 +1,188 @@
 use anchor_lang::prelude::*;
+use anchor_lang::system_program;
+use anchor_spl::token::{transfer, Token, TokenAccount};
+use borsh::BorshDeserialize;
+use solana_program::keccak::hash;
+use solana_program::secp256k1_recover::secp256k1_recover;
+use std::mem::size_of;
+
+#[error_code]
+pub enum MyError {
+    #[msg("This is a custom error message")]
+    CustomError,
+}
 
 declare_id!("9WSwbVLthCsJXABeDJcVcw4UQMYuoNLTJTLqueFXU5Q2");
 
+
+
 #[program]
-pub mod protocol_contracts_solana {
+pub mod crowdfund {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let initialized_pda = &mut ctx.accounts.pda;
+        initialized_pda.nonce = 0;
+        initialized_pda.tss_address = [
+            239, 36, 74, 232, 12, 58, 220, 53, 101, 185, 127, 45, 0, 144, 15, 163, 104, 163, 74,
+            178,
+        ];
+
         Ok(())
     }
+
+    pub fn donate(ctx: Context<Donate>, amount: u64) -> Result<()> {
+        let cpi_context = CpiContext::new(
+            ctx.accounts.system_program.to_account_info(),
+            system_program::Transfer {
+                from: ctx.accounts.signer.to_account_info().clone(),
+                to: ctx.accounts.pda.to_account_info().clone(),
+            },
+        );
+
+        system_program::transfer(cpi_context, amount)?;
+
+        Ok(())
+    }
+
+    pub fn withdraw(
+        ctx: Context<Withdraw>,
+        amount: u64,
+        signature: [u8; 64],
+        recovery_id: u8,
+        message_hash: [u8; 32],
+        nonce: u64,
+    ) -> Result<()> {
+        let pda = &mut ctx.accounts.pda;
+
+        if nonce != pda.nonce {
+            msg!("mismatch nonce");
+            return err!(MyError::CustomError);
+        }
+        let address = recover_eth_address(&message_hash, recovery_id, &signature)?; // ethereum address is the last 20 Bytes of the hashed pubkey
+        msg!("recovered address {:?}", address);
+        if address != pda.tss_address {
+            msg!("ECDSA signature error");
+            return err!(MyError::CustomError);
+        }
+
+        // transfer amount of SOL from PDA to the payer
+        pda.sub_lamports(amount)?;
+        ctx.accounts.signer.add_lamports(amount)?;
+
+        pda.nonce += 1;
+
+        Ok(())
+    }
+
+    pub fn withdraw_spl_token(
+        ctx: Context<WithdrawSPLToken>,
+        amount: u64,
+        signature: [u8; 64],
+        recovery_id: u8,
+        message_hash: [u8; 32],
+        nonce: u64,
+    ) -> Result<()> {
+        let pda = &mut ctx.accounts.pda;
+        // let program_id = &mut ctx.accounts
+        if nonce != pda.nonce {
+            msg!("mismatch nonce");
+            return err!(MyError::CustomError);
+        }
+        let address = recover_eth_address(&message_hash, recovery_id, &signature)?; // ethereum address is the last 20 Bytes of the hashed pubkey
+        msg!("recovered address {:?}", address);
+        if address != pda.tss_address {
+            msg!("ECDSA signature error");
+            return err!(MyError::CustomError);
+        }
+
+        let token = &ctx.accounts.token_program;
+        let signer_seeds: &[&[&[u8]]] = &[&[b"meta", &[ctx.bumps.pda]]];
+
+        let xfer_ctx = CpiContext::new_with_signer(
+            token.to_account_info(),
+            anchor_spl::token::Transfer {
+                from: ctx.accounts.from.to_account_info(),
+                to: ctx.accounts.to.to_account_info(),
+                authority: ctx.accounts.pda.to_account_info(),
+            },
+            signer_seeds,
+        );
+        transfer(xfer_ctx, amount)?;
+        msg!("withdraw spl token successfully");
+
+        Ok(())
+    }
+
+}
+
+fn recover_eth_address(
+    message_hash: &[u8; 32],
+    recovery_id: u8,
+    signature: &[u8; 64],
+) -> Result<[u8; 20]> {
+    let pubkey = secp256k1_recover(message_hash, recovery_id, signature)
+        .map_err(|_| ProgramError::InvalidArgument)?;
+
+    // pubkey is 64 Bytes, uncompressed public secp256k1 public key
+    let h = hash(pubkey.to_bytes().as_slice()).to_bytes();
+    let address = &h.as_slice()[12..32]; // ethereum address is the last 20 Bytes of the hashed pubkey
+    msg!("recovered address {:?}", address);
+
+    let mut eth_address = [0u8; 20];
+    eth_address.copy_from_slice(address);
+    Ok(eth_address)
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(init, payer = signer, space=size_of::<Pda>() + 8, seeds=[b"meta"], bump)]
+    pub pda: Account<'info, Pda>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Donate<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(mut)]
+    pub pda: Account<'info, Pda>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(mut)]
+    pub pda: Account<'info, Pda>,
+}
+
+#[derive(Accounts)]
+pub struct WithdrawSPLToken<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(mut, seeds = [b"meta"], bump)]
+    pub pda: Account<'info, Pda>,
+
+    #[account(mut)]
+    pub from: Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    pub to: Account<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Token>,
+}
+
+#[account]
+pub struct Pda {
+    nonce: u64,            // ensure that each signature can only be used once
+    tss_address: [u8; 20], // 20 bytes address format of ethereum
+}

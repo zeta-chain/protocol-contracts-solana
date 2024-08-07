b09affd2a9324b9dcb49846dc945fdc8cbc4ca15
Francisco de Borja Aranda Castillejo <me@fbac.dev>
2024-08-07 14:51:28 +0200

    ci: introduce formatter

---
+++ scripts/fmt.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Exit on any error
+set -e
+
+if ! command -v brew &> /dev/null
+then
+    echo "brew is required to run the script."
+    exit 1
+fi
+
+if ! command -v rustfmt &> /dev/null
+then
+    echo "rustfmt could not be found, installing..."
+    brew install rustfmt
+fi
+
+cargo fmt
+if [[ $? == 0 ]] ; then
+    echo "Code is formatted!"
+else
+    echo "An error ocurred during formatting"
+fi
